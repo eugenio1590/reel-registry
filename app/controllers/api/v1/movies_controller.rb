0abcb2c2a2513ae46c634fd123ed2561750c1b93
Eugenio Caicedo <eugeniohernandez17@gmail.com>
2024-05-02 21:23:25 -0500

    simplified controller

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -2,57 +2,57 @@ class Api::V1::MoviesController < ApplicationController
   include Authentication
   include Pagination
 
+  before_action :set_movie, only: [:show, :update, :destroy]
+
   def index
     movies = policy_scope(Movie).page(page).per(per_page)
-    options = { meta: pagination_info(movies) }
-    render json: Api::V1::MovieSerializer.new(movies, options).serializable_hash
+    render json: Api::V1::MovieSerializer.new(movies, meta: pagination_info(movies)).serializable_hash
   end
 
   def show
-    movie = policy_scope(Movie).find(params[:id])
-    render json: Api::V1::MovieSerializer.new(movie).serializable_hash
-  rescue ActiveRecord::RecordNotFound
-    head :not_found
+    render_movie(@movie)
   end
 
   def create
     movie = Movie.new(movie_params)
     authorize movie
     if movie.save
-      render json: Api::V1::MovieSerializer.new(movie).serializable_hash, status: :created
+      render_movie(movie, :created)
     else
-      render json: { errors: movie.errors.full_messages }, status: :unprocessable_entity
+      render_errors(movie)
     end
-  rescue Pundit::NotAuthorizedError
-    render json: { errors: ["not allowed to create this movie"] }, status: :forbidden
   end
 
   def update
-    movie = policy_scope(Movie).find(params[:id])
-    authorize movie
-    if movie.update(movie_params)
-      render json: Api::V1::MovieSerializer.new(movie).serializable_hash
+    authorize @movie
+    if @movie.update(movie_params)
+      render_movie(@movie)
     else
-      render json: { errors: movie.errors.full_messages }, status: :unprocessable_entity
+      render_errors(@movie)
     end
-  rescue ActiveRecord::RecordNotFound
-    head :not_found
   end
 
   def destroy
-    movie = policy_scope(Movie).find(params[:id])
-    authorize movie
-    movie.destroy
+    authorize @movie
+    @movie.destroy
     head :no_content
-  rescue ActiveRecord::RecordNotFound
-    head :not_found
-  rescue Pundit::NotAuthorizedError
-    render json: { errors: ["not allowed to delete this movie"] }, status: :forbidden
   end
 
   private
 
   def movie_params
     params.permit(:title, :release_at, :director, :genre, :synopsis, :duration, :user_id)
+  end
+
+  def set_movie
+    @movie = policy_scope(Movie).find(params[:id])
+  end
+
+  def render_movie(movie, status = :ok)
+    render json: Api::V1::MovieSerializer.new(movie).serializable_hash, status: status
+  end
+
+  def render_errors(movie)
+    render json: { errors: movie.errors.full_messages }, status: :unprocessable_entity
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::API
   include Pundit::Authorization
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from Pundit::NotAuthorizedError, with: :not_authorized
+
+  private
+
+  def not_found
+    head :not_found
+  end
+
+  def not_authorized(e)
+    render json: { errors: ["not allowed to #{e.query.chomp('?')} this movie"] }, status: :forbidden
+  end
 end
