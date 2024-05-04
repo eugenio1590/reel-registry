3739bbdfb265d5321a8fccca08be3d1efadcbefb
Eugenio Caicedo <eugeniohernandez17@gmail.com>
2024-05-03 21:17:39 -0500

    conditional user information

--- app/serializers/api/v1/movie_serializer.rb
+++ app/serializers/api/v1/movie_serializer.rb
@@ -2,4 +2,5 @@ class Api::V1::MovieSerializer
   include JSONAPI::Serializer
   
   attributes :title, :release_at, :director, :genre, :synopsis, :duration, :created_at, :updated_at
+  belongs_to :user, if: Proc.new { |record| record.user.present? }
 end
